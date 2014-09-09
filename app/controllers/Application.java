34ff81c6a00f62fa0f2386848196433456950a95
seameteo <dragonbird.tonbo@gmail.com>
2014-09-09 16:10:34 +0900

    トップページっぽいの

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,10 +2,12 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
+	
+	public static final String appTitle = "playRPG";
+	public static final String appVersion = "0.01";
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
