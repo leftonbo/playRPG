ced5c9395eb3da72af5699bc7067ca2b963991dd
seameteo <dragonbird.tonbo@gmail.com>
2014-09-09 14:53:03 +0900

    route定義

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,13 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+    
+    public static Result newGame() {
+        return TODO;
+    }
+    
+    public static Result newGameApply() {
+        return TODO;
+    }
 
 }
---
+++ app/controllers/AuthHero.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import views.html.*;
+
+public class AuthHero extends Controller {
+
+    public static Result index() {
+        return TODO;
+    }
+    
+    public static Result loginProcess() {
+        return TODO;
+    }
+    
+    
+}
