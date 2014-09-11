60121115446d2af61c9c5481e6b38d7002a2e68f
seameteo <dragonbird.tonbo@gmail.com>
2014-09-11 14:19:31 +0900

    ちょっと見やすく？

--- app/controllers/GameMain.java
+++ app/controllers/GameMain.java
@@ -8,11 +8,14 @@ public class GameMain extends Controller {
     public static Result GO_HOME = redirect(
         routes.GameMain.index()
     );
+    
+    public static String loginName;
 	
     @Security.Authenticated(MyAuthenticator.class)
     public static Result index() {
     	// TODO:ここにシーン遷移
-        return ok(gameMenu.render(request().username()));
+    	loginName = request().username();
+        return ok(loginName);
     }
 }
 
