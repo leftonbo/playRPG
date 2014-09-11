4e67f5606f9af056c8ae5afc37372c084458df7d
seameteo <dragonbird.tonbo@gmail.com>
2014-09-11 18:09:41 +0900

    シーン遷移、そしてステータス画面

--- app/controllers/GameMain.java
+++ app/controllers/GameMain.java
@@ -1,6 +1,10 @@
 package controllers;
 
+import static play.data.Form.form;
+
 import models.*;
+import models.forms.*;
+import play.data.Form;
 import play.mvc.*;
 import play.twirl.api.Html;
 import views.html.*;
@@ -16,21 +20,46 @@ public class GameMain extends Controller {
 	
     @Security.Authenticated(MyAuthenticator.class)
     public static Result index() {
-    	// TODO:ここにシーン遷移
     	loginName = request().username();
     	login = Charactor.getByName(loginName);
     	int scene = login.scene;
+    	GamePlace place = GamePlace.createByPlace(login.place);
     	
     	Html render = null;
-    	// イベントシーンへ
     	if (scene >= 1000 && scene < 2000) {
-    		
+        	// イベントシーンへ
+    		place.makeEventText(scene);
+    		String text = place.eventText
+    				.replace("{{name}}", loginName)
+    				.replace("\n", "<br>");
+    		render = gameEvent.render( place.name, place.eventName, text, place.choose);
     	} else {
-    		render = gameMenu.render(loginName);
+    		String desp = place.getDespriction()
+    				.replace("\n", "<br>");
+    		render = gameMenu.render( place.name, desp, login);
     	}
     	
     	// 選択メニュー
         return ok(render);
+    }
+
+    /**
+     * 次のシーンに移動
+     * @return
+     */
+    @Security.Authenticated(MyAuthenticator.class)
+    public static Result eventProcess() {
+    	loginName = request().username();
+    	login = Charactor.getByName(loginName);
+    	
+    	// シーンを変更
+    	Form<FormEventChoose> fc = form(FormEventChoose.class).bindFromRequest();
+    	int next = fc.get().choose;
+    	login.scene = next;
+    	// キャラクタ更新…？
+    	login.update();
+    	
+    	return GO_HOME;
     }
 }
 
